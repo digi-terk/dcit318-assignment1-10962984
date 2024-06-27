27fd2aed3b7c0233c5a218a1dbcc5baab4328bc6
SUMAILA ASARA DASMANI <sadasmani@st.ug.edu.gh>
2024-06-27 18:18:19 +0100

    Completed TriangleTypeIdentifier App

---
+++ TriangleTypeIdentifier/TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp
+{
+    class TriangleTypeIdentifier
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first side of the triangle: ");
+            int side1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second side of the triangle: ");
+            int side2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third side of the triangle: ");
+            int side3 = int.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+                Console.WriteLine("The triangle is Equilateral.");
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+                Console.WriteLine("The triangle is Isosceles.");
+            else
+                Console.WriteLine("The triangle is Scalene.");
+        }
+    }
+}
+
