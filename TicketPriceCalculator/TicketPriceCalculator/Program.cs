b667855c61a86424d642b11a7c8bf36183235ff3
SUMAILA ASARA DASMANI <sadasmani@st.ug.edu.gh>
2024-06-27 18:17:18 +0100

    Completed TicketPrice App

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp
+{
+    class TicketPriceCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+
+            Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+        }
+    }
+}
