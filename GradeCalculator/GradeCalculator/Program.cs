2f3f9d7a9ac2f2f765ec33fb4c82f213604f3fc4
SUMAILA ASARA DASMANI <sadasmani@st.ug.edu.gh>
2024-06-27 18:16:31 +0100

    Completed GradeCalculator App

---
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp
+{
+    class GradeCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numerical grade (0-100): ");
+            int grade = int.Parse(Console.ReadLine());
+
+            char letterGrade;
+
+            if (grade >= 90)
+                letterGrade = 'A';
+            else if (grade >= 80)
+                letterGrade = 'B';
+            else if (grade >= 70)
+                letterGrade = 'C';
+            else if (grade >= 60)
+                letterGrade = 'D';
+            else
+                letterGrade = 'F';
+
+            Console.WriteLine($"The letter grade is: {letterGrade}");
+        }
+    }
+}
+
